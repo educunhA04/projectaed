a806c5193db3004d3cda9242638f9ed3b734e81a
gamicmf <margaridacmf1969@gmail.com>
2023-11-04 20:13:02 +0000

    Comments are added

--- Student.h
+++ Student.h
@@ -1,39 +1,49 @@
+// Include necessary libraries and header files
 #ifndef PROJETO_STUDENT_H
 #define PROJETO_STUDENT_H
-#include <iostream>
-#include <string>
-#include <vector>
-#include <list>
 
-#include "Classes.h"
+#include <iostream>    // Include the standard input/output library
+#include <string>      // Include the string library
+#include <vector>      // Include the vector library
+#include <list>        // Include the list library
+#include "Classes.h"   // Include the Classes header file
 
-using namespace std;
+using namespace std;  // Use the standard namespace
 
+//This class define every student by name, student code and their schedule
 class Student{
+
     private:
-        string name;
-        string studentCode;
-        list<Classes> lessons; // horario de um aluno específico;
+        string name;            // Private member variable for student's name
+        string studentCode;     // Private member variable for student's code
+        list<Classes> lessons;  // Private member variable for student's lessons
 
     public:
         // Constructors
-        Student(){}
-        Student(const string& newName, const string& newStudentCode, const list<Classes>& newLessons={}){
-            this->name = newName;
-            this->studentCode = newStudentCode;
-            this->lessons = newLessons;
+        Student(){}  // Default constructor
+
+        Student(const string& newName, const string& newStudentCode, const list<Classes>& newLessons={}) {
+            // Constructor with parameters
+            this->name = newName;               // Initialize name with newName
+            this->studentCode = newStudentCode; // Initialize studentCode with newStudentCode
+            this->lessons = newLessons;         // Initialize lessons with newLessons
         }
 
+        // Overloaded less-than operator for comparing Student objects based on student code
         bool operator<(const Student& other) const {
             return studentCode < other.studentCode;
         }
 
-        string getName(){return name;}
-        string getStudentCode(){return studentCode;}
-        list<Classes> getStudentSchedule(){return lessons;}
-        void setLessons(list<Classes>& others){this->lessons=others;}
+        // Getter functions to retrieve name, studentCode, and lessons
+        string getName() { return name; }
+        string getStudentCode() { return studentCode; }
+        list<Classes> getStudentSchedule() { return lessons; }
+
+        // Setter function to update lessons
+        void setLessons(list<Classes>& others) { this->lessons = others; }
 };
 
 #endif //PROJETO_STUDENT_H
+
 
 
--- TimeType.h
+++ TimeType.h
@@ -8,32 +8,37 @@ using namespace std;
 
 class TimeType {
     private:
-        string day;
-        string startHour;
-        string classDuration;
-        string typeOfClass;
+        string startHour;       // Private member variable for start hour
+        string classDuration;   // Private member variable for class duration
+        string typeOfClass;     // Private member variable for type of class
+        string day;             // Private member variable for start day
 
     public:
-        TimeType(){}
-        TimeType(const string& newDay, const string& newStartHour, const string& newClassDuration, const string& newTypeOfClass){
-            this -> day = newDay;
-            this -> startHour = newStartHour;
-            this -> classDuration = newClassDuration;
-            this -> typeOfClass = newTypeOfClass;
-        }
-        bool operator<(const TimeType& notyet) const {
-            if (day < notyet.getDay()) return true;
-            if (day > notyet.getDay()) return false;
-        }
-        string getDay() const {return day;}
-        string getStartHour() const {return startHour;}
-        string getClassDuration() const {return classDuration;}
-        string getTypeOfClass() const {return typeOfClass;}
+        TimeType(){}    // Default constructor
 
-        string getDay() {return day;}
-        string getStartHour() {return startHour;}
-        string getClassDuration() {return classDuration;}
-        string getTypeOfClass() {return typeOfClass;}
+        TimeType(const string& newDay, const string& newStartHour, const string& newClassDuration, const string& newTypeOfClass) {
+            // Constructor with parameters
+            this->day = newDay;                   // Initialize day with newDay
+            this->startHour = newStartHour;       // Initialize startHour with newStartHour
+            this->classDuration = newClassDuration;   // Initialize classDuration with newClassDuration
+            this->typeOfClass = newTypeOfClass;   // Initialize typeOfClass with newTypeOfClass
+        }
+
+        // Overloaded less-than operator for comparing TimeType objects based on day
+        bool operator<(const TimeType& notyet) const {
+            if (day < notyet.getDay()) return true;  // Compare days
+            if (day > notyet.getDay()) return false; // Compare days
+        }
+
+        // Getter functions to retrieve day, startHour
+        string getDay() const { return day; }
+        string getStartHour() const { return startHour; }
+
+        // Overloaded getter functions (non-const version)
+        string getDay() { return day; }
+        string getStartHour() { return startHour; }
+        string getClassDuration() { return classDuration; }
+        string getTypeOfClass() { return typeOfClass; }
 
 };
 
